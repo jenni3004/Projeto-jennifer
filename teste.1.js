5fac04c057cd29f53b0fcf1338a6aca4c1cbcec9
jenni3004 <jenniferlacerda03@gmail.com>
2024-12-04 16:22:19 -0300

    Adicionar arquivos do projeto Jennifer

--- teste.1.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const baseUrl = 'https://swapi.dev/api';
-
-test('Retorna a lista de planetas', async () => {
-    const response = await request(baseUrl).get('/planets/');
-    expect(response.status).toBe(200);
-    expect(response.body.results).toBeInstanceOf(Array);
-});
